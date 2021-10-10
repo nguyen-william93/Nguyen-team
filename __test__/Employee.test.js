d4af715e310f2f4c59577146a314185497b29e09
nguyen-william93 <=>
2021-10-10 02:11:15 -0500

    create test for employee.js and code up employee.js to pass all test

---
+++ __test__/Employee.test.js
@@ -0,0 +1,20 @@
+const Employee =  require("../lib/Employee");
+const {test} = require("@jest/globals");
+
+
+test("create an Employee object", () => {
+    const employee = new Employee ("NANA", 1, "nanaiscute@gmail.com");
+
+    expect(employee.name).toBe("NANA");
+    expect(employee.id).toBe(1);
+    expect(employee.email).toBe("nanaiscute@gmail.com");
+})
+
+test("testing all the get method", () => {
+    const employee = new Employee ("NANA", 1, "nanaiscute@gmail.com");
+
+    expect(employee.getName()).toEqual(employee.name);
+    expect(employee.getId()).toEqual(employee.id);
+    expect(employee.getEmail()).toEqual(employee.email);
+    expect(employee.getRole()).toBe("Employee");
+});
---
+++ index.js
@@ -0,0 +1,28 @@
+const question = [
+    {
+        type: "text",
+        name: "name",
+        message: "Enter the manager name: "
+    },
+    {
+        type: "number",
+        name: "id",
+        message: "Enter your ID: ",
+    },
+    {
+        type: "string",
+        name: "email",
+        message: "Enter your email: "
+    },
+    {
+        type: "number",
+        name: "office_number",
+        message: "Enter your office number: "
+    },
+    {
+        type: "confirm",
+        name: "confirmAddMember",
+        message: "Would you like to add another team member?",
+        default: false
+    },
+];
