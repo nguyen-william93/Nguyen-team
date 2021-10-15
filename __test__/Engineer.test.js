e511fd15d7235bfc4866a2327503f0dec650fddb
nguyen-william93 <=>
2021-10-15 15:34:59 -0500

    All test pass on engineer

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,8 +1,22 @@
 const Engineer = require("../lib/Engineer");
-const {test} = require('@jest/globals');
+const {test} = require("@jest/globals");
 
-test("create and engineer object", () => {
-    const engineer = new Engineer("nanana", 3, "nana@gmail.com")
 
-    
+test("create a engineer object", () => {
+    const engineer = new Engineer("NANA", 1, "nanaistheBoss@gmail.com", "nguyen-william93");
+
+    expect(engineer.name).toBe("NANA");
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+test("testing get method", () => {
+    const engineer = new Engineer("NANA", 1, "nanaistheBoss@gmail.com", "nguyen-william93");
+
+    expect(engineer.getName()).toBe("NANA")
+    expect(engineer.getId()).toBe(1);
+    expect(engineer.getEmail()).toBe("nanaistheBoss@gmail.com");
+    expect(engineer.getGitHub()).toBe("nguyen-william93");
+    expect(engineer.getRole()).toBe("Engineer");
 });
